d59e9513fc2d30f79609bf6863b403969d330326
Doug Fawley <dfawley@google.com>
2025-05-02 08:24:58 -0700

    fix Cargo.toml; split examples

--- grpc/examples/multiaddr.rs
+++ grpc/examples/multiaddr.rs
@@ -5,7 +5,9 @@ use grpc::{client::ChannelOptions, inmemory};
 use tokio::sync::mpsc::error::SendError;
 use tonic::async_trait;
 
-struct Handler {}
+struct Handler {
+    id: String,
+}
 
 #[derive(Debug)]
 struct MyReqMessage(String);
@@ -20,11 +22,12 @@ impl Message for MyResMessage {}
 impl Service for Handler {
     async fn call(&self, mut request: Request) -> Response {
         let (res, tx) = Response::new();
+        let id = self.id.clone();
         tokio::task::spawn(async move {
             while let Some(req) = request.next::<MyReqMessage>().await {
                 tx.send(Box::new(MyResMessage(format!(
-                    "Server: responding to: {}; msg: {}",
-                    request.method, req.0,
+                    "Server {}: responding to: {}; msg: {}",
+                    id, request.method, req.0,
                 ))))
                 .await?;
             }
@@ -40,20 +43,41 @@ async fn main() {
     inmemory::reg();
     pick_first::reg();
 
-    // Spawn the server.
-    let lis = inmemory::Listener::new();
+    // Spawn the first server.
+    let lis1 = inmemory::Listener::new();
     let mut srv = grpc::server::Server::new();
-    srv.set_handler(Handler {});
-    let lis_clone = lis.clone();
+    srv.set_handler(Handler { id: lis1.id() });
+    let lis1_clone = lis1.clone();
     tokio::task::spawn(async move {
-        srv.serve(&lis_clone).await;
+        srv.serve(&lis1_clone).await;
         println!("serve returned for listener 1!");
     });
 
-    println!("Creating channel for {}", lis.target());
+    // Spawn the second server.
+    let lis2 = inmemory::Listener::new();
+    let mut srv = grpc::server::Server::new();
+    srv.set_handler(Handler { id: lis2.id() });
+    let lis2_clone = lis2.clone();
+    tokio::task::spawn(async move {
+        srv.serve(&lis2_clone).await;
+        println!("serve returned for listener 2!");
+    });
+
+    // Spawn the third server.
+    let lis3 = inmemory::Listener::new();
+    let mut srv = grpc::server::Server::new();
+    srv.set_handler(Handler { id: lis3.id() });
+    let lis3_clone = lis3.clone();
+    tokio::task::spawn(async move {
+        srv.serve(&lis3_clone).await;
+        println!("serve returned for listener 3!");
+    });
+
+    let target = String::from("inmemory:///dummy");
+    println!("Creating channel for {target}");
     let chan_opts =
         ChannelOptions::default().transport_registry(transport::GLOBAL_TRANSPORT_REGISTRY.clone());
-    let chan = grpc::client::Channel::new(lis.target().as_str(), None, None, chan_opts);
+    let chan = grpc::client::Channel::new(target.as_str(), None, None, chan_opts);
 
     let (req, tx) = Request::new("hi", None);
     let mut res = chan.call(req).await;
@@ -72,5 +96,7 @@ async fn main() {
     while let Some(resp) = res.next::<MyResMessage>().await {
         println!("CALL RESPONSE: {}", resp.0);
     }
-    lis.close().await;
+    lis1.close().await;
+    lis2.close().await;
+    lis3.close().await;
 }
