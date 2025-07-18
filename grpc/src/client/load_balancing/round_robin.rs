f4c1855626bec1fde38a01c76fc1f3e04a33729a
Cathy Zhao <cathyjzhao@google.com>
2025-07-18 18:11:53 +0000

    fixed based on review

--- grpc/src/client/load_balancing/round_robin.rs
+++ grpc/src/client/load_balancing/round_robin.rs
@@ -34,6 +34,7 @@ use tokio::{
 use once_cell::sync::Lazy;
 use rand::{self, rngs::StdRng, seq::SliceRandom, thread_rng, Rng, RngCore, SeedableRng};
 use serde::{Deserialize, Serialize};
+use std::sync::Once;
 use tokio::time::sleep;
 use tonic::{async_trait, metadata::MetadataMap};
 
@@ -41,14 +42,15 @@ use tonic::{async_trait, metadata::MetadataMap};
 mod test;
 
 pub static POLICY_NAME: &str = "round_robin";
+static WRAPPED_PICKFIRST_NAME: &str = "wrapped_pick_first";
+static START: Once = Once::new();
 
 struct RoundRobinBuilder {}
 
 impl LbPolicyBuilder for RoundRobinBuilder {
     fn build(&self, options: LbPolicyOptions) -> Box<dyn LbPolicy> {
-        super::GLOBAL_LB_REGISTRY.add_builder(WrappedPickFirstBuilder {});
-        let resolver_update_sharder = ResolverUpdateSharderStruct {
-            builder: GLOBAL_LB_REGISTRY.get_policy("wrapped_pick_first").unwrap(),
+        let resolver_update_sharder = EndpointSharder {
+            builder: WrappedPickFirstBuilder::new(),
         };
         let lb_policy = Box::new(ChildManager::new(Box::new(resolver_update_sharder)));
         Box::new(RoundRobinPolicy {
@@ -65,9 +67,6 @@ impl LbPolicyBuilder for RoundRobinBuilder {
     }
 }
 
-/**
-Struct for round_robin.
-*/
 struct RoundRobinPolicy {
     child_manager: Box<ChildManager<Endpoint>>,
     work_scheduler: Arc<dyn WorkScheduler>,
@@ -84,18 +83,10 @@ impl RoundRobinPolicy {
             .iter()
             .flat_map(|ep| ep.addresses.clone())
             .collect();
-
-        // Remove duplicates.
-        let mut uniques = HashSet::new();
-        addresses.retain(|e| uniques.insert(e.clone()));
-
-        // TODO(easwars): Implement address family interleaving as part of
-        // the dualstack implementation.
-
         addresses
     }
 
-    fn move_to_transient_failure(&mut self, channel_controller: &mut dyn ChannelController) {
+    fn move_to_transient_failure(&self, channel_controller: &mut dyn ChannelController) {
         let err = format!(
             "last seen resolver error: {:?}, last seen connection error: {:?}",
             self.last_resolver_error, self.last_connection_error,
@@ -106,142 +97,13 @@ impl RoundRobinPolicy {
         });
         channel_controller.request_resolution();
     }
-}
 
-/// Register round robin as a LbPolicy.
-pub fn reg() {
-    super::GLOBAL_LB_REGISTRY.add_builder(RoundRobinBuilder {});
-}
-
-struct WrapperPickFirstPolicy {
-    pick_first: Box<dyn LbPolicy>,
-}
-
-struct WrappedPickFirstBuilder {}
-
-pub static WRAPPED_PICKFIRST_NAME: &str = "wrapped_pick_first";
-
-impl LbPolicyBuilder for WrappedPickFirstBuilder {
-    fn build(&self, options: LbPolicyOptions) -> Box<dyn LbPolicy> {
-        pick_first::reg();
-        Box::new(WrapperPickFirstPolicy {
-            pick_first: GLOBAL_LB_REGISTRY.get_policy("pick_first").unwrap().build(
-                LbPolicyOptions {
-                    work_scheduler: options.work_scheduler,
-                },
-            ),
-        })
-    }
-
-    fn name(&self) -> &'static str {
-        WRAPPED_PICKFIRST_NAME
-    }
-}
-
-impl LbPolicy for WrapperPickFirstPolicy {
-    fn resolver_update(
-        &mut self,
-        update: ResolverUpdate,
-        config: Option<&LbConfig>,
-        channel_controller: &mut dyn ChannelController,
-    ) -> Result<(), Box<dyn Error + Send + Sync>> {
-        let mut wrapped_channel_controller = WrappedController::new(channel_controller);
-        let result = self
-            .pick_first
-            .resolver_update(update, None, &mut wrapped_channel_controller);
-        if wrapped_channel_controller.idle_update {
-            self.exit_idle(&mut wrapped_channel_controller);
+    fn update_state(&self, channel_controller: &mut dyn ChannelController) {
+        if self.child_manager.has_updated() {
+            if let Some(pick_update) = self.child_manager.aggregate_states() {
+                channel_controller.update_picker(pick_update);
+            }
         }
-        result
-    }
-
-    fn subchannel_update(
-        &mut self,
-        subchannel: Arc<dyn Subchannel>,
-        state: &SubchannelState,
-        channel_controller: &mut dyn ChannelController,
-    ) {
-        let mut wrapped_channel_controller = WrappedController::new(channel_controller);
-        self.pick_first
-            .subchannel_update(subchannel, state, &mut wrapped_channel_controller);
-        if wrapped_channel_controller.idle_update {
-            self.exit_idle(&mut wrapped_channel_controller);
-        }
-    }
-
-    fn work(&mut self, channel_controller: &mut dyn ChannelController) {
-        let mut wrapped_channel_controller = WrappedController::new(channel_controller);
-        self.pick_first.work(&mut wrapped_channel_controller);
-        if wrapped_channel_controller.idle_update {
-            self.exit_idle(&mut wrapped_channel_controller);
-        }
-    }
-
-    fn exit_idle(&mut self, channel_controller: &mut dyn ChannelController) {
-        self.pick_first.exit_idle(channel_controller);
-    }
-}
-
-struct WrappedController<'a> {
-    channel_controller: &'a mut dyn ChannelController,
-    idle_update: bool,
-}
-
-impl<'a> WrappedController<'a> {
-    fn new(channel_controller: &'a mut dyn ChannelController) -> Self {
-        Self {
-            channel_controller,
-            idle_update: false,
-        }
-    }
-}
-
-impl ChannelController for WrappedController<'_> {
-    fn new_subchannel(&mut self, address: &Address) -> Arc<dyn Subchannel> {
-        let subchannel = self.channel_controller.new_subchannel(address);
-        subchannel
-    }
-
-    fn update_picker(&mut self, update: LbState) {
-        let update_clone = update.clone();
-        self.channel_controller.update_picker(update);
-        if update_clone.connectivity_state == ConnectivityState::Idle {
-            self.idle_update = true;
-        } else {
-            self.idle_update = false;
-        }
-    }
-
-    fn request_resolution(&mut self) {
-        self.channel_controller.request_resolution();
-    }
-}
-
-struct ResolverUpdateSharderStruct {
-    builder: Arc<dyn LbPolicyBuilder>,
-}
-
-impl ResolverUpdateSharder<Endpoint> for ResolverUpdateSharderStruct {
-    fn shard_update(
-        &self,
-        resolver_update: ResolverUpdate,
-    ) -> Result<Box<dyn Iterator<Item = ChildUpdate<Endpoint>>>, Box<dyn Error + Send + Sync>> {
-        let mut hashmap = HashMap::new();
-        for endpoint in resolver_update.endpoints.clone().unwrap().iter() {
-            let child_update = ChildUpdate {
-                child_identifier: endpoint.clone(),
-                child_policy_builder: self.builder.clone(),
-                // Create new resolver update with particular endpoint.
-                child_update: ResolverUpdate {
-                    attributes: resolver_update.attributes.clone(),
-                    endpoints: Ok(vec![endpoint.clone()]),
-                    service_config: resolver_update.service_config.clone(),
-                    resolution_note: resolver_update.resolution_note.clone(),
-                },
-            };
-            hashmap.insert(endpoint.clone(), child_update);
-        }
-        Ok(Box::new(hashmap.into_values()))
     }
 }
 
@@ -274,20 +136,16 @@ impl LbPolicy for RoundRobinPolicy {
                 let result =
                     self.child_manager
                         .resolver_update(cloned_update, config, channel_controller);
-
-                if self.child_manager.has_updated() {
-                    if let Some(pick_update) = self.child_manager.aggregate_states() {
-                        channel_controller.update_picker(pick_update);
-                    }
-                }
+                self.update_state(channel_controller);
                 self.addresses = new_addresses;
             }
             Err(error) => {
-                self.move_to_transient_failure(channel_controller);
-                if self.child_manager.has_updated() {
-                    if let Some(pick_update) = self.child_manager.aggregate_states() {
-                        channel_controller.update_picker(pick_update);
-                    }
+                if self.addresses.is_empty()
+                    || self.child_manager.prev_state != ConnectivityState::Ready
+                {
+                    self.move_to_transient_failure(channel_controller);
+                } else {
+                    self.update_state(channel_controller);
                 }
             }
         }
@@ -302,31 +160,181 @@ impl LbPolicy for RoundRobinPolicy {
     ) {
         self.child_manager
             .subchannel_update(subchannel, state, channel_controller);
-        if self.child_manager.has_updated() {
-            if let Some(pick_update) = self.child_manager.aggregate_states() {
-                channel_controller.update_picker(pick_update);
-            }
-        }
+        self.update_state(channel_controller);
     }
 
     fn work(&mut self, channel_controller: &mut dyn ChannelController) {
-        // Build a new subchannel list with the most recent addresses received
-        // from the name resolver. This will start connecting from the first
-        // address in the list.
         self.child_manager.work(channel_controller);
-        if self.child_manager.has_updated() {
-            if let Some(pick_update) = self.child_manager.aggregate_states() {
-                channel_controller.update_picker(pick_update);
-            }
-        }
+        self.update_state(channel_controller);
     }
 
     fn exit_idle(&mut self, channel_controller: &mut dyn ChannelController) {
         self.child_manager.exit_idle(channel_controller);
-        if self.child_manager.has_updated() {
-            if let Some(pick_update) = self.child_manager.aggregate_states() {
-                channel_controller.update_picker(pick_update);
-            }
+        self.update_state(channel_controller);
+    }
+}
+
+/// Register round robin as a LbPolicy.
+pub fn reg() {
+    START.call_once(|| {
+        GLOBAL_LB_REGISTRY.add_builder(RoundRobinBuilder {});
+    });
+}
+
+struct WrappedPickFirstPolicy {
+    pick_first: Box<dyn LbPolicy>,
+}
+
+struct WrappedPickFirstBuilder {}
+
+impl WrappedPickFirstPolicy {
+    fn new(options: LbPolicyOptions) -> Box<dyn LbPolicy> {
+        pick_first::reg();
+        Box::new(WrappedPickFirstPolicy {
+            pick_first: GLOBAL_LB_REGISTRY
+                .get_policy(pick_first::POLICY_NAME)
+                .unwrap()
+                .build(LbPolicyOptions {
+                    work_scheduler: options.work_scheduler,
+                }),
+        })
+    }
+}
+
+impl LbPolicyBuilder for WrappedPickFirstBuilder {
+    fn build(&self, options: LbPolicyOptions) -> Box<dyn LbPolicy> {
+        pick_first::reg();
+        Box::new(WrappedPickFirstPolicy {
+            pick_first: GLOBAL_LB_REGISTRY
+                .get_policy(pick_first::POLICY_NAME)
+                .unwrap()
+                .build(LbPolicyOptions {
+                    work_scheduler: options.work_scheduler,
+                }),
+        })
+    }
+
+    fn name(&self) -> &'static str {
+        WRAPPED_PICKFIRST_NAME
+    }
+}
+
+impl WrappedPickFirstBuilder {
+    fn new() -> Arc<dyn LbPolicyBuilder> {
+        Arc::new(WrappedPickFirstBuilder {})
+    }
+}
+
+/*
+This wrapped Pick First policy ensures that whenever a Pick First policy goes IDLE, it will
+exit_idle and immediately try to start connecting to subchannels again.
+This is because Round Robin attempts to maintain a connection to every endpoint at all times.
+ */
+impl LbPolicy for WrappedPickFirstPolicy {
+    fn resolver_update(
+        &mut self,
+        update: ResolverUpdate,
+        config: Option<&LbConfig>,
+        channel_controller: &mut dyn ChannelController,
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
+        let mut wrapped_channel_controller = WrappedController::new(channel_controller);
+        let result = self
+            .pick_first
+            .resolver_update(update, None, &mut wrapped_channel_controller);
+        if wrapped_channel_controller.policy_is_idle {
+            self.exit_idle(&mut wrapped_channel_controller);
         }
+        result
+    }
+
+    fn subchannel_update(
+        &mut self,
+        subchannel: Arc<dyn Subchannel>,
+        state: &SubchannelState,
+        channel_controller: &mut dyn ChannelController,
+    ) {
+        let mut wrapped_channel_controller = WrappedController::new(channel_controller);
+        self.pick_first
+            .subchannel_update(subchannel, state, &mut wrapped_channel_controller);
+        if wrapped_channel_controller.policy_is_idle {
+            self.exit_idle(&mut wrapped_channel_controller);
+        }
+    }
+
+    fn work(&mut self, channel_controller: &mut dyn ChannelController) {
+        let mut wrapped_channel_controller = WrappedController::new(channel_controller);
+        self.pick_first.work(&mut wrapped_channel_controller);
+        if wrapped_channel_controller.policy_is_idle {
+            self.exit_idle(&mut wrapped_channel_controller);
+        }
+    }
+
+    fn exit_idle(&mut self, channel_controller: &mut dyn ChannelController) {
+        self.pick_first.exit_idle(channel_controller);
+    }
+}
+
+/*
+This wrapped channel controller keeps track of whether a policy
+went idle, thus signaling that whether the policy should exit_idle or not.
+ */
+struct WrappedController<'a> {
+    channel_controller: &'a mut dyn ChannelController,
+    policy_is_idle: bool,
+}
+
+impl<'a> WrappedController<'a> {
+    fn new(channel_controller: &'a mut dyn ChannelController) -> Self {
+        Self {
+            channel_controller,
+            policy_is_idle: false,
+        }
+    }
+}
+
+impl ChannelController for WrappedController<'_> {
+    fn new_subchannel(&mut self, address: &Address) -> Arc<dyn Subchannel> {
+        self.channel_controller.new_subchannel(address)
+    }
+
+    fn update_picker(&mut self, update: LbState) {
+        if update.connectivity_state == ConnectivityState::Idle {
+            self.policy_is_idle = true;
+        } else {
+            self.policy_is_idle = false;
+        }
+        self.channel_controller.update_picker(update);
+    }
+
+    fn request_resolution(&mut self) {
+        self.channel_controller.request_resolution();
+    }
+}
+
+struct EndpointSharder {
+    builder: Arc<dyn LbPolicyBuilder>,
+}
+
+impl ResolverUpdateSharder<Endpoint> for EndpointSharder {
+    fn shard_update(
+        &self,
+        resolver_update: ResolverUpdate,
+    ) -> Result<Box<dyn Iterator<Item = ChildUpdate<Endpoint>>>, Box<dyn Error + Send + Sync>> {
+        let mut endpoint_to_child = HashMap::new();
+        for endpoint in resolver_update.endpoints.clone().unwrap().iter() {
+            let child_update = ChildUpdate {
+                child_identifier: endpoint.clone(),
+                child_policy_builder: self.builder.clone(),
+                // Create new resolver update with particular endpoint.
+                child_update: ResolverUpdate {
+                    attributes: resolver_update.attributes.clone(),
+                    endpoints: Ok(vec![endpoint.clone()]),
+                    service_config: resolver_update.service_config.clone(),
+                    resolution_note: resolver_update.resolution_note.clone(),
+                },
+            };
+            endpoint_to_child.insert(endpoint.clone(), child_update);
+        }
+        Ok(Box::new(endpoint_to_child.into_values()))
     }
 }
